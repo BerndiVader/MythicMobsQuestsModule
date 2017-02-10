a2d9a29124b51a298cfca317a8a4bd995d21ff7b
BuildTools <kasermandel@gmail.com>
2017-02-10 09:20:29 +0100

    fixed for 2.4.5-4.0.0 & 1.8.8 - 1.11.2

--- readme.md
+++ readme.md
@@ -1,17 +1,2 @@
-# Quest Addon for MythicMobs 2.5.1 or higher
+# Quest Addon for MythicMobs 2.4.5 to 4.0.0 and Spigot 1.8.3 - 1.11.2
 
-Copy the jar file into your servers Quests/modules/ folder. Restart the server.
-
-Use QuestMMKillObjective260.jar for MythicMobs 2.6 or QuestMMKillObjective250.jar for 2.5 Versions.
-
-Example config:
-
-          custom-objectives:
-            custom1:
-              name: Kill MythicMobs Objective
-              count: 5
-              data:
-                Objective Name: Kill some MythicMobs
-                Internal Mobnames: mm_zombie,mm_skeleton,mm_creeper
-          finish-event: GoodJob
-          death-event: DeathFail
--- src/com/gmail/berndivader/questsMythicMobsKillObjective/QuestMMKillObjective.java
+++ src/com/gmail/berndivader/questsMythicMobsKillObjective/QuestMMKillObjective.java
@@ -2,18 +2,25 @@ package com.gmail.berndivader.questsMythicMobsKillObjective;
 
 import java.util.Map;
 
+import org.apache.commons.lang.ArrayUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 
 import me.blackvein.quests.CustomObjective;
 import me.blackvein.quests.Quest;
 import me.blackvein.quests.Quester;
 import me.blackvein.quests.Quests;
-import net.elseland.xikage.MythicMobs.API.Bukkit.Events.MythicMobDeathEvent;
 
 
 public class QuestMMKillObjective extends CustomObjective implements Listener {
+
+	private String strMMVer;
+	private int mmVer;
+	
 	
 	public QuestMMKillObjective() {
 		setName("Kill MythicMobs Objective");
@@ -25,19 +32,37 @@ public class QuestMMKillObjective extends CustomObjective implements Listener {
 		setShowCount(true);
 		setCountPrompt("How many MythicMobs to kill");
 		setDisplay("%Objective Name%, Counter: %count%");
+		strMMVer = Bukkit.getServer().getPluginManager().getPlugin("MythicMobs").getDescription().getVersion();
+		mmVer = Integer.valueOf(strMMVer.replaceAll("\\.", ""));
 	}
 	
 	@EventHandler
-	public void onMythicMobsKillEvent (MythicMobDeathEvent e) {
-		if (!(e.getKiller() instanceof Player)) {return;}
-		Player p = (Player)e.getKiller();
+	public void onMythicMobDeathEvent (EntityDeathEvent e) {
+		if (!(e.getEntity() instanceof LivingEntity)) return;
+		LivingEntity bukkitentity = e.getEntity();
+		if (!(bukkitentity.getKiller() instanceof Player)) return;
+		String mobtype = null;
+		Player p;
+		if ((mmVer > 244 && mmVer < 252)) {
+			net.elseland.xikage.MythicMobs.Mobs.ActiveMob am = 
+					net.elseland.xikage.MythicMobs.MythicMobs.inst().getAPI().getMobAPI().getMythicMobInstance(bukkitentity);
+			if (am==null) return;
+			mobtype = am.getType().getInternalName();
+		} else if (mmVer > 259 && mmVer < 2511) {
+			io.lumine.xikage.mythicmobs.mobs.ActiveMob am = 
+					io.lumine.xikage.mythicmobs.MythicMobs.inst().getMobManager().getMythicMobInstance(bukkitentity);
+			if (am==null) return;
+			mobtype = am.getType().getInternalName();
+		}
+		if (mobtype.isEmpty()) return;
+		p = bukkitentity.getKiller();
 		Quester qp = Quests.getInstance().getQuester(p.getUniqueId());
 		if (qp.currentQuests.isEmpty()) return;
 		for (Quest q : qp.currentQuests.keySet()) {
 			Map<?, ?> m = QuestMMKillObjective.getDatamap(p, this, q);
 			if (m == null) continue;
-			String kt = m.get("Internal Mobnames").toString();
-			if (kt.contains(e.getMobType().getInternalName())) {QuestMMKillObjective.incrementObjective(p, this, 1, q);}
+			String[] kt = m.get("Internal Mobnames").toString().split(",");
+			if (ArrayUtils.contains(kt, mobtype)) {QuestMMKillObjective.incrementObjective(p, this, 1, q);}
 		}
 	}
 }
