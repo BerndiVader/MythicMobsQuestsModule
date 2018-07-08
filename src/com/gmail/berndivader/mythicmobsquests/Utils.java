ffe28e7296ad0af13d2b222986b774d936108604
BerndiVader <kasermandel@gmail.com>
2018-07-08 11:48:04 +0200

    support for MythicMobs 4.4

--- src/com/gmail/berndivader/mythicmobsquests/Utils.java
+++ src/com/gmail/berndivader/mythicmobsquests/Utils.java
@@ -1,28 +1,26 @@
 package com.gmail.berndivader.mythicmobsquests;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import io.lumine.xikage.mythicmobs.MythicMobs;
-import io.lumine.xikage.mythicmobs.adapters.AbstractEntity;
 import io.lumine.xikage.mythicmobs.adapters.bukkit.BukkitAdapter;
-import io.lumine.xikage.mythicmobs.compatibility.CompatibilityManager;
 import io.lumine.xikage.mythicmobs.drops.DropManager;
-import io.lumine.xikage.mythicmobs.drops.MythicDropTable;
+import io.lumine.xikage.mythicmobs.drops.DropMetadata;
+import io.lumine.xikage.mythicmobs.drops.DropTable;
+import io.lumine.xikage.mythicmobs.drops.LootBag;
+import io.lumine.xikage.mythicmobs.mobs.GenericCaster;
 import io.lumine.xikage.mythicmobs.mobs.MobManager;
 import io.lumine.xikage.mythicmobs.util.types.RangedDouble;
 import me.blackvein.quests.Quests;
@@ -75,9 +73,8 @@ public class Utils {
 
 	public static boolean createAndDropItemStack(String[]arr1,String itemMarker,int amount,Player player,boolean notify,boolean stackable) {
 		boolean bl1=false;
-		World w=player.getWorld();
-		AbstractEntity trigger=BukkitAdapter.adapt(player);
 		HashMap<String,Integer>lm=new HashMap<>();
+		List<String>nm=new ArrayList<String>();
 		for(int i1=0;i1<arr1.length;i1++) {
 			String itemtype;
 			if (arr1[i1].contains(":")) {
@@ -87,54 +84,19 @@ public class Utils {
 			} else {
 				itemtype=arr1[i1];
 			}
-			MythicDropTable dt=null;
+			nm.add(itemtype);
+			DropTable dt;
 			if(!dropmanager.isPresent()) getDropManager();
 			if(dropmanager.isPresent()) {
-				dt=dropmanager.get().getDropTable(itemtype).orElse(new MythicDropTable(Arrays.asList(itemtype),null,null,null,null));
+				dt=new DropTable("QuestDrop","QuestDrop",nm);
 			} else {
 				Bukkit.getLogger().info("DropManager wasnt present.");
 				return false;
 			}
-			if (dt.hasConditions()) dt.conditions=new ArrayList<>();
 			for (int a=0;a<amount;a++) {
-				dt.parseTable(trigger);
-				if (dt.getExp()>0) {
-					player.giveExp(dt.getExp());
-					lm.put(str_exp,lm.containsKey(str_exp)?lm.get(str_exp)+dt.getExp():dt.getExp());
-				}
-				if (dt.heroesexp>0&&CompatibilityManager.Heroes!=null) {
-					lm.put(str_hexp,lm.containsKey(str_hexp)?lm.get(str_hexp)+dt.getHeroesExp():dt.getHeroesExp());
-					CompatibilityManager.Heroes.giveHeroesExp(null,player,dt.heroesexp);
-				}
-				if (dt.mcmmoexp>0&&CompatibilityManager.mcMMO!=null) {
-					lm.put(str_mexp,lm.containsKey(str_mexp)?lm.get(str_mexp)+dt.getMcMMOExp():dt.getMcMMOExp());
-					CompatibilityManager.mcMMO.giveExp(player,dt.getChampionsExp(),"unarmed");
-				}
-				if (dt.getMoney()>0&&MythicMobs.inst().getCompatibility().getVault().isPresent()) {
-					lm.put(str_money,lm.containsKey(str_money)?lm.get(str_money)+(int)dt.getMoney():(int)dt.getMoney());
-					MythicMobs.inst().getCompatibility().getVault().get().giveMoney(player,dt.getMoney());
-				}
-				for (ItemStack is:dt.getDrops()) {
-					if (is==null||is.getType()==Material.AIR) continue;
-					bl1=true;
-					String nn=is.hasItemMeta()&&is.getItemMeta().hasDisplayName()?is.getItemMeta().getDisplayName():is.getType().toString();
-					nn+=": ";
-					lm.put(nn,lm.containsKey(nn)?lm.get(nn)+is.getAmount():is.getAmount());
-					if (itemMarker!=null) NMSUtils.setMeta(is,str_questitem,itemMarker);
-					if (!stackable) {
-						UUID uuid=UUID.randomUUID();
-						String most=Long.toString(uuid.getMostSignificantBits()),least=Long.toString(uuid.getLeastSignificantBits());
-						NMSUtils.setMeta(is,"RandomMost",most);
-						NMSUtils.setMeta(is,"RandomLeast",least);
-					}
-					NMSUtils.setMeta(is,"MythicMobsItem","true");
-					if ((player.getInventory().firstEmpty())>-1) {
-						player.getInventory().addItem(is.clone());
-						player.updateInventory();
-					} else {
-						w.dropItem(player.getLocation(),is.clone());
-					}
-				}
+				GenericCaster dropper=new GenericCaster(BukkitAdapter.adapt(player));
+		        LootBag lb=dt.generate(new DropMetadata(dropper,dropper.getEntity()));
+		        lb.drop(BukkitAdapter.adapt(player).getLocation());
 			}
 			if (notify) {
 				String ll=ChatColor.DARK_GREEN+"";
@@ -146,6 +108,7 @@ public class Utils {
 				player.sendMessage(ll);
 			}
 		}
+		nm.clear();
 		return bl1;
 	}
 	
